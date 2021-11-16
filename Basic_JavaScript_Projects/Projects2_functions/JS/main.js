2339ad52e038924b1554501a5e17d5f840656b84
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-16 12:11:03 -0500

    More

--- Basic_JavaScript_Projects/Projects2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Projects2_functions/JS/main.js
@@ -1,5 +1,5 @@
 function My_Second_Function() {
     var str = "Yep, this is happening";
-    var result = str.fontcolor("purple");
-    document.getElementById("Purple_Text").innerHTML = result;
+    var result = str.fontcolor("pink");
+    document.getElementById("Pink_Text").innerHTML = result;
 }
