10eeace2fba8e98f11d03471a61917e25416fea7
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-16 14:29:54 -0500

    Update main.js

--- Basic_JavaScript_Projects/Projects2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Projects2_functions/JS/main.js
@@ -4,9 +4,9 @@ function My_Second_Function() {
     document.getElementById("Pink_Text").innerHTML = result;
 }
 
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot of this book!";
-    document.getElementById("Concatenate").innerHTML = sentence;
+function myFunction() { //Defining function and namaing it
+    var sentence = "I am learning"; //Defining a variable and naming it
+    sentence += " a lot of this book!";//Concatenate the variable sentence with the second part of the sentence
+    document.getElementById("Concatenate").innerHTML = sentence; //Putting the value of Concatenate into the HTML element with Sentence
 }
 
