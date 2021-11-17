1a428308d5405b14c9dc3c793c0e6dde25b3c119
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-17 16:57:32 -0500

    More

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -23,3 +23,13 @@ function more_Math() {
     var simple_Math = (1 + 2) * 10 / 2 -5;
     document.getElementById("math5").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
+
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("math6").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+}
+
+function negation_Operator() {
+    var x = 10;
+    document.getElementById("math7").innerHTML = -x;
+}
