e44ce701fdc0056a5c3250fc240a02b1d7f69bef
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-17 15:43:28 -0500

    More

---
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,20 @@
+function addition() {
+   var add = 5 + 10;
+   document.getElementById("math").innerHTML = "5 + 10 = " + add;
+}
+
+function subtraction() {
+    var x = 5
+    var y = 2
+    document.getElementById("math2").innerHTML = x - y;
+}
+
+function multiplication() {
+    var simple_Math = 6 * 8;
+    document.getElementById("math3").innerHTML = simple_Math;
+}
+
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("math4").innerHTML = simple_Math;
+}
