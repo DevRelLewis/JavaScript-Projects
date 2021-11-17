ca4e3c98c1151c99558d1ed31135cc530df5e752
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-17 16:07:33 -0500

    More

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -18,3 +18,8 @@ function division() {
     var simple_Math = 48 / 6;
     document.getElementById("math4").innerHTML = simple_Math;
 }
+
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("math5").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
+}
