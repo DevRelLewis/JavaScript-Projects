87a8b23ebab1144032478da696400c662a029eb0
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-17 12:13:12 -0500

    More

---
+++ Basic_JavaScript_Projects/Projects2_functions/JS/new_Function.js
@@ -0,0 +1,10 @@
+function newFunction(cat, dog) {
+    document.getElementById("text").innerHTML = cat * dog;
+}
+newFunction(3,4)
+
+function addition_Function() {
+    var addition = 2 + 2;
+    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+}
+addition_Function()
