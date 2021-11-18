128a3fdc461f6c53f48e089be24ee2ad5886fca0
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-18 11:26:03 -0500

    Update main.js

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -4,6 +4,11 @@ document.write(10 < 2);
 document.write(10 == 10);
 document.write(3 == 11);
 
+document.write(4 > 2 && 8 > 6);
+document.write(5 > 7 && 7 > 5);
+document.write(3 > 5 || 7 > 6);
+document.write(3 > 4 || 6 > 7);
+
 
 X = 10;
 Y = 10;
