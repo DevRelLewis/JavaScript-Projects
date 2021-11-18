c61d51905fa30c9a2025a7c4af844e0ad6d1f0f0
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-18 14:13:35 -0500

    More

--- Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -4,3 +4,16 @@ function Ride_Function() {
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough"
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford","Pinto", 1970, "Mustard");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML = "Erik drives a " + Erik.Vehicle_Color + " manufactured in " + Erik.Vehicle_Model;
+}
