c14ab13e13e6c306195af58413c5df9fc53cb688
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-18 11:15:18 -0500

    Update main.js

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,4 +1,21 @@
 document.write("Super" + "monkey");
+document.write(10 > 2);
+document.write(10 < 2);
+document.write(10 == 10);
+document.write(3 == 11);
+
+
+X = 10;
+Y = 10;
+console.log(X === Y);
+
+X = 2;
+Y = "2";
+console.log(X === Y);
+
+console.log(2 * 2);
+console.log(1 > 2);
+console.log(10 == 12);
 
 function another_Function() {
     document.getElementById("Test").innerHTML = (-3E310);
@@ -7,5 +24,7 @@ function another_Function() {
 function yet_another_Function() {
     document.getElementById("Test2").innerHTML = (3E310);
 }
+
+
 
 
