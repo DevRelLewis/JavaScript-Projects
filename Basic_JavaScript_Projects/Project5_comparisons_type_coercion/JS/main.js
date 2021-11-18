16c34bb9f4b4efa8e4431a0bc3fe53e7dd6cf1f1
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-18 10:42:38 -0500

    More

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,11 @@
 document.write("Super" + "monkey");
 
+function another_Function() {
+    document.getElementById("Test").innerHTML = (-3E310);
+}
+
+function yet_another_Function() {
+    document.getElementById("Test2").innerHTML = (3E310);
+}
+
 
