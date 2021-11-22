7389a22abcb89937879e54e816a383804ac375b5
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-22 13:17:36 -0500

    More

--- Basic_JavaScript_Projects/Prjoect10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Prjoect10_loops_arrays/JS/main.js
@@ -7,3 +7,14 @@ function count_To_Ten() {
     }
     document.getElementById("Counting_to_Ten").innerHTML = Digit;
 }
+
+var Instruments = ["Guitar", "Drums", "Piano" , "Bass" , "Violin" , 
+                    "Trumpet" , "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
