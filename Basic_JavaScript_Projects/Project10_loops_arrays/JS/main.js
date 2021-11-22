659b5ff92f71a5589f3d12f4c964491ce1d0093a
Lewis Meyers <meyerslewis193@gmail.com>
2021-11-22 17:32:29 -0500

    More

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,14 @@
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make +this.model + ".";
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
 var X = 82;
 document.write(X);
 {
